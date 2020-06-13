0281c247a680f1936860f748cf5348127b050692
GitGudCode440 <62549763+GitGudCode440@users.noreply.github.com>
2020-06-13 17:26:55 +0300

    Create PlayerDetection.gd

---
+++ PlayerDetection.gd
@@ -0,0 +1,61 @@
+extends "res://Characters/TemplateCharacter.gd"
+
+const MAX_DETECTION_RANGE = 440
+const FOV_TOLERANCE = 13
+
+const RED = Color(1,0.25,0.25)
+const WHITE = Color(1,1,1)
+
+var Player
+
+func _ready():
+	
+	Player = get_node("/root").find_node("Player", true, false)
+	
+	
+	pass
+	
+func _process(delta):
+	
+	
+	if Player_in_FOV() and Player_in_LOS():
+		$Light2D.color = RED
+		get_tree().call_group("suspicion_meter", "player_seen")
+	else:
+		$Light2D.color = WHITE
+
+
+func Player_in_FOV():
+	
+	
+	
+	var npc_facing_direction = Vector2(1, 0).rotated(global_rotation)
+	var direction_to_Player = (Player.position - global_position).normalized()
+	
+	
+	
+	if abs(direction_to_Player.angle_to(npc_facing_direction)) < deg2rad(FOV_TOLERANCE):
+		return true
+	else:
+		return false
+	
+func Player_in_LOS():
+	
+	
+	var space = get_world_2d().direct_space_state
+	var LOS_obstacle = space.intersect_ray(global_position, Player.position, [self], collision_mask)
+	
+	var distance_to_Player = Player.global_position.distance_to(global_position)
+	var Player_in_Range = distance_to_Player < MAX_DETECTION_RANGE
+	
+	
+	
+	if LOS_obstacle.collider == Player and Player_in_Range:
+		return true
+	else:
+		return false
+	
+	
+
+
+	
